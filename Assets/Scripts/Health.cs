a7695bae0df2c35233cdbe6bf7096f8a3a19656d
latreta <yveshenr@gmail.com>
2020-08-04 17:40:43 -0300

    trying out damage over time

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+    [SerializeField]
+    private Healthbar healthbar;
+    float timeElapsed = 0f;
+
+    public int CurrentHealth { get; private set; }
+
+    private void OnEnable()
+    {
+        CurrentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if(timeElapsed >= 5f)
+        {
+            TakeDamage(-5);
+            timeElapsed = 0f;
+        }
+    }
+
+    private void TakeDamage(int amount)
+    {
+        Debug.Log("Taking damage");
+        CurrentHealth += amount;
+        float currentHealthPct = (float)CurrentHealth / (float)maxHealth;
+        healthbar.UpdateSize(currentHealthPct);
+    }
+}
---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField]
+    private Image bar;
+
+    public void UpdateSize(float percentage)
+    {
+        bar.fillAmount = percentage;
+    }
+
+
+}
